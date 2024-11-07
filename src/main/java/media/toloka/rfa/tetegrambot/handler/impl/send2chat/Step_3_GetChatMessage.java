e93426056cbc31b2dbc092f9c15ccb0afabd31c0
ysv <ysv@toloka.kiev.ua>
2024-11-07 15:24:27 +0200

    Робота Телеграму з активними чатами ефірів. Відпрацював логіку роботи. Повідомлення в чати прямих ефірів не надсилаю. Начебто вдалося зареєструвати користувача з телеграму та привʼязати до аккаунту портала телеграм користувача. Реєстрація користувача та привʼязка начебто працює.  Залишилося подивитися правильність повідомлень та додати збереження файлів у сховище з Телеграму.

--- src/main/java/media/toloka/rfa/tetegrambot/handler/impl/send2chat/Step_3_GetChatMessage.java
+++ src/main/java/media/toloka/rfa/tetegrambot/handler/impl/send2chat/Step_3_GetChatMessage.java
@@ -2,8 +2,10 @@ package media.toloka.rfa.tetegrambot.handler.impl.send2chat;
 // Надсилаємо повідомлення в чат прямого ефіру
 // перелік прямих ефірів беремо з чату
 import lombok.extern.slf4j.Slf4j;
+import media.toloka.rfa.media.messanger.model.ChatMessage;
 import media.toloka.rfa.media.messanger.service.MessangerService;
 import media.toloka.rfa.radio.client.service.ClientService;
+import media.toloka.rfa.radio.model.Clientdetail;
 import media.toloka.rfa.tetegrambot.enums.ConversationState;
 import media.toloka.rfa.tetegrambot.handler.UserRequestHandler;
 import media.toloka.rfa.tetegrambot.helper.KeyboardHelper;
@@ -12,9 +14,14 @@ import media.toloka.rfa.tetegrambot.model.UserSession;
 import media.toloka.rfa.tetegrambot.service.TelegramService;
 import media.toloka.rfa.tetegrambot.service.UserSessionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboard;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
+
+import java.util.Date;
+
+import static media.toloka.rfa.media.messanger.model.enumerate.EChatRecordType.RECORD_TYPE_TEXT;
 
 @Slf4j
 @Component
@@ -25,14 +32,20 @@ public class Step_3_GetChatMessage extends UserRequestHandler {
     @Autowired
     private ClientService clientService;
 
+    private SimpMessagingTemplate template;
+
     private final TelegramService telegramService;
     private final KeyboardHelper keyboardHelper;
     private final UserSessionService userSessionService;
 
-    public Step_3_GetChatMessage(TelegramService telegramService, KeyboardHelper keyboardHelper, UserSessionService userSessionService) {
+    public Step_3_GetChatMessage(TelegramService telegramService,
+                                 KeyboardHelper keyboardHelper,
+                                 UserSessionService userSessionService,
+                                 SimpMessagingTemplate template) {
         this.telegramService = telegramService;
         this.keyboardHelper = keyboardHelper;
         this.userSessionService = userSessionService;
+        this.template = template;
     }
 
     @Override
@@ -58,12 +71,32 @@ public class Step_3_GetChatMessage extends UserRequestHandler {
         if (messangerService.GetChatRoomById(userRequest.getUserSession().getRFA_Chat_Choose().getId()).getRoomOnlineStatus()) {
             telegramService.sendMessage(userRequest.getChatId(),
                     "Ви надсилаєте повідомлення до чату: \n"+userRequest.getUserSession().getRFA_Chat_Choose().getRoomname());
+            // Формуємо повідомлення дл чату RFA
+            ChatMessage cmsg = new ChatMessage();
+            cmsg.setBody(userRequest.getUpdate().getMessage().getText()); // Тіло повідомлення
+            cmsg.setRoomuuid(userRequest.getUserSession().getRFA_Chat_Choose().getUuid());
+            Clientdetail cd = clientService.GetUserFromTelegram(userRequest.getUpdate().getMessage().getFrom().getId().toString());
+            cmsg.setFromuuid(cd.getUuid());
+            String sFrom = "Telegram - ";
+            if (cd.getCustname() != null ) sFrom += cd.getCustname()+" ";
+            if (cd.getCustsurname() != null) sFrom += cd.getCustsurname();
+            cmsg.setFromname(sFrom);
+            cmsg.setRtype(RECORD_TYPE_TEXT.ordinal());
+            cmsg.setSend(new Date());
+            try {
+                this.template.convertAndSend("/topic/"+cmsg.getRoomuuid(), cmsg);
+            } catch (Exception e) {
+                log.info("Step_3_GetChatMessage: PutChatPrivateMessage Exception");
+                e.printStackTrace();
+            }
+//            inmsg.setRtype(RECORD_TYPE_TEXT.ordinal());
+            messangerService.SaveMessageFromChat(cmsg);
 
             telegramService.sendMessage(userRequest.getChatId(),
                     "Введіть повідомлення та надішліть\nДля завершення натисніть \"Скасувати\"",
                     replyKeyboardMarkup);
             // формуємо перелік чатів і нумеруємо їх
-            log.info(userRequest.getUpdate().getMessage().getText());
+//            log.info(userRequest.getUpdate().getMessage().getText());
         } else {// Чат вийшов з онлайну
             // Встановлюємо стан для наступного кроку
             log.info("Прямий ефір завершився. Виходимо з чату.");
