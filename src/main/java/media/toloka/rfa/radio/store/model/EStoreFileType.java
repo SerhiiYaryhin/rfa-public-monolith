66c4090a25d81d9acbed523a2684d93472dac238
ysv <ysv@toloka.kiev.ua>
2025-02-22 12:25:48 +0200

    Додав STORE_PHOTOALBUMS("Фото Альбому");

--- src/main/java/media/toloka/rfa/radio/store/model/EStoreFileType.java
+++ src/main/java/media/toloka/rfa/radio/store/model/EStoreFileType.java
@@ -2,15 +2,17 @@ package media.toloka.rfa.radio.store.model;
 
 public enum EStoreFileType {
 
-    STORE_FILE("Файл"),
-    STORE_DOCUMENT("Документ"),
-    STORE_TRACK("Трек"),
-    STORE_ALBUMCOVER("Обкладинка альбому"),
-    STORE_POSTCOVER("Обкладинка посту"),
-    STORE_PHOTO("Фото профайлу"),
-    STORE_PODCASTCOVER("Обкладинка подкасту"),
-    STORE_EPISODECOVER_FREE("Обкладинка епізоду"), // не використовую. Замісь неї використовую STORE_PODCASTCOVER
-    STORE_EPISODETRACK("Трек епізоду");
+    STORE_FILE("Файл"), // 0
+    STORE_DOCUMENT("Документ"), // 1
+    STORE_TRACK("Трек"), // 2
+    STORE_ALBUMCOVER("Обкладинка альбому"), // 3
+    STORE_POSTCOVER("Обкладинка посту"), // 4
+    STORE_PHOTO("Фото профайлу"), // 5
+    STORE_PODCASTCOVER("Обкладинка подкасту"), // 6
+    STORE_EPISODECOVER_FREE("Обкладинка епізоду"), // 7 // не використовую. Замісь неї використовую STORE_PODCASTCOVER
+    STORE_EPISODETRACK("Трек епізоду"), // 8
+    STORE_RESERV_1("Резерв"), // 9
+    STORE_PHOTOALBUMS("Фото Альбому"); // 10
     public final String label;
 
     private EStoreFileType(String label) {
