8eb9b5ffc9ab7595ccb4c4e5be3b3fbc4b307eb4
ysv <ysv@toloka.kiev.ua>
2025-03-07 11:28:20 +0200

    бавлюся TTS

--- src/main/java/media/toloka/rfa/rpc/service/RPCSpeachService.java
+++ src/main/java/media/toloka/rfa/rpc/service/RPCSpeachService.java
@@ -11,11 +11,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import static media.toloka.rfa.radio.model.enumerate.EHistoryType.History_NewsSendToTTS;
 import static media.toloka.rfa.radio.model.enumerate.EHistoryType.History_StationCreate;
 
+@Profile("tts")
 @Service
 public class RPCSpeachService {
 
