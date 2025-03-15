e1d23fd8b6cd16e60ae63bc64bc5faf764eebf3c
ysv <ysv@toloka.kiev.ua>
2025-03-15 20:17:59 +0200

    Працюю з паролем коримтувача Libretime

--- src/main/resources/ShellFiles/toRadio/toRadio.sh
+++ src/main/resources/ShellFiles/toRadio/toRadio.sh
@@ -7,7 +7,7 @@ cp ~/rfa/src/main/resources/ShellFiles/toRadio/toRadio.sh ~/bin/
 rm ~/bin/py/*
 cp ~/rfa/src/main/resources/ShellFiles/toRadio/py/* ~/bin/py
 cd ~/bin
-source toRadio/bin/activate
-cd py
-python3 toRadio.py
-deactivate
+#source toRadio/bin/activate
+#cd py
+#python3 toRadio.py
+#deactivate
