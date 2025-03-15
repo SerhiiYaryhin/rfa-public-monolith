44fcb2a07346a7ee5a8b9dcd2701339a7ce84e0e
ysv <ysv@toloka.kiev.ua>
2025-03-15 18:18:21 +0200

    Працюю з паролем коримтувача Libretime

--- src/main/resources/ShellFiles/toRadio/RFAstart.sh
+++ src/main/resources/ShellFiles/toRadio/RFAstart.sh
@@ -3,7 +3,9 @@ cd /home/toradio/rfa
 git switch current
 git pull
 
-cp ~/rfa/src/main/resources/ShellFiles/toRadio/* ~/bin/
+cp ~/rfa/src/main/resources/ShellFiles/toRadio/toRadio.sh ~/bin/
+rm ~/bin/py/*
+cp ~/rfa/src/main/resources/ShellFiles/toRadio/py/* ~/bin/py
 cd ~/bin
 source toRadio/bin/activate
 python3 toRadio.py
