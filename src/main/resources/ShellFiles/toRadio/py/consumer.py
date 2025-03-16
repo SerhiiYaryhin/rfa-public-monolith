8b7d315cc1fda9bc0f9414a71a16240402c5d2ad
ysv <ysv@toloka.kiev.ua>
2025-03-16 17:10:51 +0200

    Розбираюся з паролями користувача libretime для трансляції

--- src/main/resources/ShellFiles/toRadio/py/consumer.py
+++ src/main/resources/ShellFiles/toRadio/py/consumer.py
@@ -4,7 +4,7 @@
 import os
 import pika
 import json
-import hexdump # https://stackoverflow.com/questions/12214801/print-a-string-as-hexadecimal-bytes
+import hexdump  # https://stackoverflow.com/questions/12214801/print-a-string-as-hexadecimal-bytes
 from config_loader import config
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
@@ -25,17 +25,18 @@ credentials = pika.PlainCredentials(rabbitmq_user, rabbitmq_password)
 
 # Встановлення з'єднання
 connection = pika.BlockingConnection(
-        pika.ConnectionParameters(
-            host=rabbitmq_host,
-            port=rabbitmq_port,
-            virtual_host=rabbitmq_vhost,  # Віртуальний хост
-            credentials=credentials
-        )
+    pika.ConnectionParameters(
+        host=rabbitmq_host,
+        port=rabbitmq_port,
+        virtual_host=rabbitmq_vhost,  # Віртуальний хост
+        credentials=credentials
+    )
 )
 channel = connection.channel()
 
 # Декларація черг
 channel.queue_declare(queue=input_queue, durable=True)
+
 
 # Завантажуємо приватний ключ із файлу
 def load_private_key(guiServer):
@@ -46,7 +47,8 @@ def load_private_key(guiServer):
         private_key = RSA.import_key(fileRead)
         # return RSA.import_key(key_bytes)
         return private_key
-        #return RSA.import_key(key_bytes)
+        # return RSA.import_key(key_bytes)
+
 
 # Функція для розшифрування повідомлення
 def decrypt_rsa(encrypted_message, private_key):
@@ -55,9 +57,10 @@ def decrypt_rsa(encrypted_message, private_key):
     decrypted_data = cipher_rsa.decrypt(encrypted_message)
     return decrypted_data.decode()
 
+
 # Зберігаєио приватний ключ від gui сервера
 def SavePrivateKey(news_rpc_obj):
-    #breakpoint()
+    # breakpoint()
     ldir = os.path.expanduser(locateDir)
 
     # Create the directory
@@ -71,35 +74,38 @@ def SavePrivateKey(news_rpc_obj):
     except Exception as e:
         print(f"An error occurred: {e}")
 
-
-    with open(ldir+"/"+news_rpc_obj["guiserver"]+".priv", "w") as private_file:
+    with open(ldir + "/" + news_rpc_obj["guiserver"] + ".priv", "w") as private_file:
         private_file.write(news_rpc_obj["key"])
-        #private_file.write(news_rpc_obj["key"].encode("utf-8"))
+        # private_file.write(news_rpc_obj["key"].encode("utf-8"))
     return 0
 
 
 # віправляємо зі сторе в ефір
 def ToRadio(news_rpc_obj):
-    print (news_rpc_obj)
-    criptopsw =  news_rpc_obj["cpsw"]
-    #breakpoint()
+    print(news_rpc_obj)
+    criptopsw = news_rpc_obj["cpsw"]
+    # breakpoint()
     # print (criptopsw)
-    guiserver =  news_rpc_obj["guiserver"]
+    guiserver = news_rpc_obj["guiserver"]
+    print("====================== guiserver")
     print(guiserver)
-    baseSiteAddress =  news_rpc_obj["baseSiteAddress"]
-    newsUUID =  news_rpc_obj["newsStoreUUID"]
-    username =  news_rpc_obj["username"]
-    mainport =  news_rpc_obj["mainport"]
-    mainpoint =  news_rpc_obj["mainpoint"]
+    baseSiteAddress = news_rpc_obj["baseSiteAddress"]
+    newsUUID = news_rpc_obj["newsStoreUUID"]
+    username = news_rpc_obj["username"]
+    mainport = news_rpc_obj["mainport"]
+    mainpoint = news_rpc_obj["mainpoint"]
 
     # private_key = load_private_key(guiserver)
     # 1️⃣ Читаємо приватний ключ із файлу (PEM-формат)
     with open(os.path.expanduser(locateDir) + "/" + guiServer + ".priv", "r") as key_file:
-        private_key = RSA.import_key(key_file.read())
-        # 3️⃣ Розшифровуємо дані
-        cipher_rsa = PKCS1_OAEP.new(private_key)
-        decrypted_message = cipher_rsa.decrypt(criptopsw)
+        pkey = key_file.read()
+    print("====================== pkey")
+    print(pkey)
+    private_key = RSA.import_key(pkey)
 
+    # 3️⃣ Розшифровуємо дані
+    cipher_rsa = PKCS1_OAEP.new(private_key)
+    decrypted_message = cipher_rsa.decrypt(criptopsw)
 
     # print("====================== criptopsw")
     # print (criptopsw)
@@ -111,25 +117,26 @@ def ToRadio(news_rpc_obj):
     # формуємо командну строку
     cmd = "ffmpeg -re -v quiet -stats -i https://front.rfa.toloka.media/store/audio/" + news_rpc_obj["newsUUID"]
     +   "https://front.rfa.toloka.media:" + news_rpc_obj["mainport"]
-    + "/" +  news_rpc_obj["mainpoint"]
+    + "/" + news_rpc_obj["mainpoint"]
+
+    print(cmd)
 
 
-
-    print (cmd)
 # Функція обробки повідомлення
 def process_toRadio(news_rpc_obj):
-    #breakpoint()
+    # breakpoint()
     match news_rpc_obj["job"]:
         case "toRadiokey":
-             SavePrivateKey(news_rpc_obj)
+            SavePrivateKey(news_rpc_obj)
         case "toRadio":
-             ToRadio(news_rpc_obj)
+            ToRadio(news_rpc_obj)
         case _:
             print("Якісь фігня прелетіла")
 
     print("🔄 Очікування повідомлень... Натисніть CTRL+C для виходу.")
     # Обробити нештатні ситуації
     return 0;
+
 
 # Функція зворотного виклику для обробки повідомлення
 def callback(ch, method, properties, body):
@@ -142,17 +149,18 @@ def callback(ch, method, properties, body):
     news_rpc_obj["rc"] = process_toRadio(news_rpc_obj)
 
     # Перетворюємо назад у JSON
-    #output_json = json.dumps(news_rpc_obj)
+    # output_json = json.dumps(news_rpc_obj)
 
     # Надсилаємо у вихідну чергу
-    #output_queue = news_rpc_obj["front"]["server"]
+    # output_queue = news_rpc_obj["front"]["server"]
 
-#    channel.queue_declare(queue=news_rpc_obj["front"]["server"], durable=True)
-    #print(f"📤 output_queue: {rabbitmq_vhost} - {output_queue}")
-    #ch.basic_publish(exchange="", routing_key=output_queue, body=output_json)
-    #print(f"📤 Відправлено у {output_queue}: {output_json}")
+    #    channel.queue_declare(queue=news_rpc_obj["front"]["server"], durable=True)
+    # print(f"📤 output_queue: {rabbitmq_vhost} - {output_queue}")
+    # ch.basic_publish(exchange="", routing_key=output_queue, body=output_json)
+    # print(f"📤 Відправлено у {output_queue}: {output_json}")
 
     ch.basic_ack(delivery_tag=method.delivery_tag)  # Підтвердження отримання
+
 
 # Підписка на вхідну чергу
 channel.basic_consume(queue=input_queue, on_message_callback=callback)
