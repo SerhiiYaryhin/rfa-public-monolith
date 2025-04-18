9eabca99aab7a324f14e6625ce03cc56f00989bd
ysv <ysv@toloka.kiev.ua>
2025-04-18 10:33:06 +0300

    Проміжне збереження. Розбираюся з основами бухгалтерії.

--- src/main/java/media/toloka/rfa/account/model/Documents/AccCompletedWorkDocument.java
+++
@@ -1,6 +0,0 @@
-package media.toloka.rfa.account.model.Documents;
-// виконана робота
-// конкретна робота, що була виконана. Наприклад - текст -> голос, робота радіостанції тощо
-
-public class AccCompletedWorkDocument {
-}
--- src/main/java/media/toloka/rfa/account/model/Documents/AccInvoiceDocument.java
+++
@@ -1,5 +0,0 @@
-package media.toloka.rfa.account.model.Documents;
-// Рахунок на сплату
-
-public class AccInvoiceDocument {
-}
