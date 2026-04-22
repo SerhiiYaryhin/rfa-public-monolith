4d74f9b5b8b56215422e1aa18a518aee57dd1c2b
ollama coder <coder@tts.rfa>
2026-04-22 12:44:07 +0300

    Implement paginated global storage for administrators

--- src/main/java/media/toloka/rfa/radio/admin/AdminStoreController.java
+++ src/main/java/media/toloka/rfa/radio/admin/AdminStoreController.java
@@ -12,9 +12,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
@@ -44,17 +46,23 @@ public class AdminStoreController {
     final Logger logger = LoggerFactory.getLogger(AdminStoreController.class);
 
 
-    @GetMapping(value = "/admin/storage")
+    @GetMapping(value = {"/admin/storage", "/admin/storage/{pageNumber}"})
     public String getAdminStore(
+            @PathVariable(required = false) Integer pageNumber,
             Model model ) {
         Users user = clientService.GetCurrentUser();
         if (user == null) {
             return "redirect:/";
         }
 
+        int currentPage = (pageNumber == null) ? 0 : pageNumber;
+        Page<Store> pageStore = storeService.GetStorePage(currentPage, 20);
 
-        List<Store> storeList = storeService.GetAll();
-        model.addAttribute("storeList", storeList );
+        model.addAttribute("storeList", pageStore.getContent());
+        model.addAttribute("totalPages", pageStore.getTotalPages());
+        model.addAttribute("currentPage", currentPage);
+        model.addAttribute("linkPage", "/admin/storage/");
+        
         return "/admin/storage";
     }
 }
