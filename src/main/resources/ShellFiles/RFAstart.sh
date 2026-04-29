af86e65ef5a39863ec1922dd6ec2f9af9d433d4a
ysv <ysv@toloka.kiev.ua>
2026-04-29 11:11:29 +0300

    Вніс зміни в шелл файл для автоматичного запуску порталу на сервері. Тимчасово запускаю з гілки current.

--- src/main/resources/ShellFiles/RFAstart.sh
+++ src/main/resources/ShellFiles/RFAstart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /home/rfa/prodact
-git switch prodact
+git switch current
+#git switch prodact
 git pull
 
 cp ~/prodact/src/main/resources/ShellFiles/* ~/bin/
@@ -8,4 +9,5 @@ cp ~/prodact/src/main/resources/ShellFiles/* ~/bin/
 /bin/sh -c 'until ping -c1 8.8.8.8; do sleep 5; done;'
 /bin/sh -c 'until ping -c4 pg.rfa; do sleep 1; done;'
 export QRFA=/rfa
-java -jar -Dspring.profiles.active=default,Front build/libs/RFA-1.0.0.war
+QRFA=/rfa TELEGRAMBOTNAME=TestTolokaMediaBot TELEGRAMBOTKEY=7319689514:AAEwGibd0JOdvqOvZnG0STL_hAGffEqQdCA SPRING_PROFILES_ACTIVE=current,Front,Telegram ./gradlew bootRun --args='--server.port=3080 --spring.datasource.url=jdbc:postgresql://pg.rfa:5432/rfa'
+#java -jar -Dspring.profiles.active=default,Front build/libs/RFA-1.0.0.war
