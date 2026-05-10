8aac08599a8ecd0e9b9183d964c2b0dcf1b5face
ysv <ysv@toloka.kiev.ua>
2026-05-10 22:57:17 +0300

    Борюся з http в метаданних open graph

--- src/main/java/media/toloka/rfa/config/GlobalModelAttributes.java
+++ src/main/java/media/toloka/rfa/config/GlobalModelAttributes.java
@@ -1,21 +1,49 @@
 package media.toloka.rfa.config;
 
 import jakarta.servlet.http.HttpServletRequest;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @ControllerAdvice
 public class GlobalModelAttributes {
 
-    @Value("${media.toloka.rfa.server.globalname:https://rfa.toloka.media}")
-    private String siteUrl;
-
     @ModelAttribute("siteUrl")
     public String getSiteUrl(HttpServletRequest request) {
-        // Використовуємо фактичний домен запиту, якщо він відрізняється від конфігурації
-        String requestUrl = request.getRequestURL().toString();
-        String requestDomain = requestUrl.substring(0, requestUrl.indexOf(request.getRequestURI()));
-        return requestDomain;
+        // ServletUriComponentsBuilder автоматично перевірить:
+        // 1. Заголовки X-Forwarded-Proto (якщо налаштовано стратегію native)
+        // 2. Порти та домени, надіслані через проксі
+
+        return ServletUriComponentsBuilder.fromCurrentContextPath()
+                .build()
+                .toUriString();
     }
 }
+
+
+
+
+
+
+
+
+//
+//import jakarta.servlet.http.HttpServletRequest;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.web.bind.annotation.ControllerAdvice;
+//import org.springframework.web.bind.annotation.ModelAttribute;
+//
+//@ControllerAdvice
+//public class GlobalModelAttributes {
+//
+//    @Value("${media.toloka.rfa.server.globalname:https://rfa.toloka.media}")
+//    private String siteUrl;
+//
+//    @ModelAttribute("siteUrl")
+//    public String getSiteUrl(HttpServletRequest request) {
+//        // Використовуємо фактичний домен запиту, якщо він відрізняється від конфігурації
+//        String requestUrl = request.getRequestURL().toString();
+//        String requestDomain = requestUrl.substring(0, requestUrl.indexOf(request.getRequestURI()));
+//        return requestDomain;
+//    }
+//}
