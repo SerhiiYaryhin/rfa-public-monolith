b7c9fbeb1231d12811b1dbaffac6968ab91f9057
ysv <ysv@toloka.kiev.ua>
2026-05-10 23:38:58 +0300

    Борюся з open graph в метаданних

--- src/main/java/media/toloka/rfa/radio/root/RootController.java
+++ src/main/java/media/toloka/rfa/radio/root/RootController.java
@@ -146,7 +146,19 @@ public class RootController {
     @RequestMapping(value={"/robots.txt", "/robot.txt"})
     @ResponseBody
     public String getRobotsTxt() {
-        return "User-agent: *\nAllow: /\nDisallow: /admin/\n\nUser-agent: facebookexternalhit\nAllow: /\n\nUser-agent: Facebot\nAllow: /\n";
+        return """
+            User-agent: *
+            Allow: /
+            Disallow: /admin/
+            
+            User-agent: facebookexternalhit
+            Allow: /
+            
+            User-agent: Facebot
+            Allow: /
+            """;
+
+        //"User-agent: * \nAllow: / \nDisallow: /admin/ \n\nUser-agent: facebookexternalhit \nAllow: / \n\nUser-agent: Facebot\nAllow: /\n";
     }
 
 }
