1a008bd886d76803078e01b3bac231eb71fd5e50
YSV with ollama coder <ysv@toloka.kiev.ua>
2026-05-13 19:07:07 +0300

    fix: resolve compilation error in PodcastDropPostFileController and include missed fixes

--- src/main/java/media/toloka/rfa/podcast/fileupload/PodcastDropPostFileController.java
+++ src/main/java/media/toloka/rfa/podcast/fileupload/PodcastDropPostFileController.java
@@ -13,6 +13,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,6 +22,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.Map;
 
 import static media.toloka.rfa.radio.store.model.EStoreFileType.*;
 
